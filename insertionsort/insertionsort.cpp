c0478693e4e7fbbc7d648fbd6467bdcae2e9afc2
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-03-24 15:58:28 +0700

    untuk menampilkan data ke- layar

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -20,5 +20,10 @@ void input() {
 	cout << "======================" << endl;
 	cout << "Masukkan elemen array" << endl;
 	cout << "======================" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
 
 }
