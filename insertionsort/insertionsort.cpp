8add539e945e329f2c7bc878c9bc5c727541ca30
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-03-24 16:08:40 +0700

    menampilkan n pass

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -39,6 +39,11 @@ void insertionsort() {
 			arr[j + 1] = arr[j];
 			j--;
 		}
+		arr[j + 1] = temp;
 
-
+		cout << "\nPass " << i << ": ";
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " ";
+		}
 	}
+}
